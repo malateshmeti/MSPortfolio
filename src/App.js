bcfb4082c41c5e339d32ea915f8f80df6d5215d4
Malatesh <msmeti4501@gmail.com>
2025-09-09 22:51:50 +0530

    Test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-     <h1>Welcome to Malatesh Meti Portfolio 🚀</h1>
+     <h1>Welcome to Malatesh Meti Portfolio.</h1>
     <p>Soon, you’ll get to see a fully responsive portfolio of Malatesh.</p>
     </div>
   );
